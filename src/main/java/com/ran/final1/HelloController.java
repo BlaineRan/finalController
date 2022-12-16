ac19da3fc776ed7e628c5aff5658c08491ab9f14
BlaineRan <1819153268@qq.com>
2022-12-16 22:11:23 +0800

    Ran 2022/12/16 fixed

--- src/main/java/com/ran/final1/HelloController.java
+++ src/main/java/com/ran/final1/HelloController.java
@@ -1,2 +1,15 @@
-package com.ran.final1;public class HelloController {
+package com.ran.final1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/getHello")
+    @ResponseBody
+    String getHello(){
+        return "hello world";
+    }
 }
