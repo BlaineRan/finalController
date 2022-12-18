6c1a70c22cd188a81508e44958b586e193590f78
BlaineRan <1819153268@qq.com>
2022-12-18 22:55:25 +0800

    Ran 2022/12/18 fixed

--- src/main/java/com/ran/final1/controller/UserController.java
+++ src/main/java/com/ran/final1/controller/UserController.java
@@ -1,24 +1,37 @@
 package com.ran.final1.controller;
 
-import com.ran.final1.domain.Users;
+import com.ran.final1.domain.User;
 import com.ran.final1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@CrossOrigin(allowCredentials = "true",originPatterns = "*")
 public class UserController {
     @Autowired
     public UserService userService;
 
     @RequestMapping("/getUserList")
     @ResponseBody
-    List<Users> getUserList(){
+    List<User> getUserList(){
         return userService.queryAllUsers();
     }
 
+    @RequestMapping("/login")
+    @ResponseBody
+    Map<String,String> login(@RequestBody(required = false) Map<String,String> data){
+        System.out.println(data);
+
+        String account = data.get("account");
+        String password = data.get("password");
+        return userService.login(account,password);
+    }
 }
--- src/main/java/com/ran/final1/service/UserService.java
+++ src/main/java/com/ran/final1/service/UserService.java
@@ -1,23 +1,47 @@
 package com.ran.final1.service;
 
-import com.ran.final1.domain.Users;
-import com.ran.final1.mapper.UsersMapper;
+import com.ran.final1.domain.User;
+import com.ran.final1.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
     @Autowired
-    public UsersMapper usersMapper;
+    public UserMapper userMapper;
 
     /**
      * 查询所有用户
      */
-    public List<Users> queryAllUsers(){
-        return usersMapper.selectAll();
+    public List<User> queryAllUsers(){
+        return userMapper.selectAll();
     }
 
+    public User queryUserByAccount(String account){
+        return userMapper.selectByUserAccount(account).get(0);
+    }
 
+    public Map<String,String> login(String account,String password){
+        Map<String,String> loginResult = new HashMap<>();
+        List<User> userList = userMapper.selectByUserAccount(account);
+        User user = null;
+        if(userList.size() != 0){
+            user = userList.get(0);
+        }
+        if(user != null){
+            if(user.getUserPassword().equals(password)){
+                loginResult.put("status","ok");
+                loginResult.put("type",user.getUserType());
+            }else{
+                loginResult.put("status","error");
+            }
+        }else{
+            loginResult.put("status","error");
+        }
+        return loginResult;
+    }
 }
